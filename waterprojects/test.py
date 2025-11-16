0e287dc743862a8f17241c1f229ac12073db67f5
BestBaconAround <officialcammy14@gmail.com>
2025-11-15 23:47:24 -0700

    sdfksd

--- waterprojects/test.py
+++ waterprojects/test.py
@@ -26,3 +26,28 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(-100.462, abs=0.001)
     assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, abs=0.001)
     assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
+
+from waterproject import pressure_loss_from_fittings
+
+def test_pressure_loss_from_fittings():
+    assert pressure_loss_from_fittings(0.0, 3) == approx(0.0, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0.0, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
+
+from waterproject import reynolds_number
+
+def test_reynolds_number():
+    assert reynolds_number(0.048692, 0.0) == approx(0.0, abs=1)
+    assert reynolds_number(0.048692, 1.65) == approx(80069, abs=1)
+    assert reynolds_number(0.048692, 1.75) == approx(84922, abs=1)
+    assert reynolds_number(0.286870, 1.65) == approx(471729, abs=1)
+    assert reynolds_number(0.286870, 1.75) == approx(500318, abs=1)
+
+from waterproject import pressure_loss_from_pipe_reduction
+
+def test_pressure_loss_from_pipe_reduction():
+    assert pressure_loss_from_pipe_reduction(0.28687, 0.0, 1, 0.048692) == approx(0, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, abs=0.001)
--- waterprojects/waterproject.py
+++ waterprojects/waterproject.py
@@ -17,3 +17,40 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     denominator = 2000 * pipe_diameter
     pressure_loss = - numerator / denominator
     return pressure_loss
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    if fluid_velocity == 0 or quantity_fittings == 0:
+        return 0.0
+    water_density = 998.2
+    velocity_squared = fluid_velocity * fluid_velocity
+    numerator = 0.04 * water_density * velocity_squared * quantity_fittings
+    denominator = 2000
+    pressure_loss = - numerator / denominator
+    return pressure_loss
+
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    if fluid_velocity == 0:
+        return 0.0
+    water_density = 998.2
+    water_dynamics_viscosity = 0.0010016
+    numerator = water_density * hydraulic_diameter * fluid_velocity
+    reynolds = numerator / water_dynamics_viscosity
+    return reynolds
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+    if fluid_velocity == 0:
+        return 0.0
+    water_density = 998.2 
+    ratio = larger_diameter / smaller_diameter
+    k = (0.1 + 50 / reynolds_number) * (ratio**4 - 1)
+    pressure_loss = -k * water_density * (fluid_velocity ** 2) / 2000
+    return pressure_loss
+
+def main():
+    tower_height = float(input("Height of water towers (meters)"))
+    tank_wall_height = float(input("Length of supply pipe from tank to lot (meters)"))
+    length_tank_to_lot = float(input("Length of supply pipe from tank to lot (meters)"))
+    fitting_count = int(input("Number of 90 angles in supply pipe: "))
+    length_lot_to_house = float(input("Length of pipe from supply to house (meters)"))
+
+    
